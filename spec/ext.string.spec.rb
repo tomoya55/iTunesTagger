c812e183f5b022443c1fd05e4fb28e08d4a48494
Tomoya Hirano <hiranotomoya@gmail.com>
2009-05-31 22:59:05 +0900

    -

--- spec/ext.string.spec.rb
+++ spec/ext.string.spec.rb
@@ -11,4 +11,11 @@ describe ITunesTagger::Ext::StringExtension do
     str.md5!
     str.should == "ea703e7aa1efda0064eaa507d9e8ab7e"
   end
+  
+  it "should encode using des and can decode correctly" do
+    str = 'message'
+    str.des_encode('key').should_not == str
+    str.des_encode('key').des_decode('key').should == str
+    lambda { str.des_encode('key').des_decode('key2') }.should raise_error('bad decrypt')
+  end
 end
